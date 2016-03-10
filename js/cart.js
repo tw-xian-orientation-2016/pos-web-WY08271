3af57c5c1304b05d6ddd3eec68b8ca76353ab3a8
WY08271 <wy08271@outlook.com>
2016-03-11 04:21:29 +0800

    modify cart.js

--- js/cart.js
+++ js/cart.js
@@ -61,7 +61,7 @@ function updateCart(receiptItems) {
   var itemHTML = '';
   receiptItems.forEach(function(receiptItem) {
     var item = receiptItem.item;
-    itemHTML += '<tr id="' + item.id + '"><td class="goods">' + item.name + '</td><td class="price">' + item.price + '/' + item.unit + '</td><td class="count"><input class="count-input" type="text" value="1"/></td><td class="subTotal">' +
+    itemHTML += '<tr id="' + item.id + '"><td class="goods">' + item.name + '</td><td class="price">' + item.price + '/' + item.unit + '</td><td class="count"><input class="count-input" type="text" value="1" data-id="' + item.id + '"/></td><td class="subTotal">' +
       item.price * receiptItem.count + '</td><td><button class="btn btn-default delete-button" type="submit" data-id="' + item.id + '">删除</button></td></tr>';
   });
   $('tbody').append(itemHTML);
