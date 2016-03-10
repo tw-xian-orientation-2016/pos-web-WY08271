b69a8fac278715e6f17c1e002b22bb8c02bad1b6
WY08271 <wy08271@outlook.com>
2016-03-10 15:05:15 +0800

    modify updateSelectCount(), updatePriceTotal()

--- js/cart.js
+++ js/cart.js
@@ -2,14 +2,70 @@ $(document).ready(function() {
   var receiptItems = JSON.parse(localStorage.getItem("receiptItems"));
 
   updateCart(receiptItems);
+  updateSelectCount(receiptItems);
+  updatepriceTotal(receiptItems);
+
+  $(".delete-button").click(function() {
+    var id = parseInt(this.getAttribute("data-id"));
+    $(this).parent().parent().hide();
+
+    deleteReceiptItem(id, receiptItems);
+  });
+
+  $(".navbar-link").click(function() {
+    saveReceiptList();
+  });
 });
 
-function updateCart(receiptItems){
+function saveReceiptList() {
+  var receiptItems = JSON.parse(localStorage.getItem("receiptItems"));
+  var selectCount = updateSelectCount(receiptItems);
+  var priceTotal = updatepriceTotal(receiptItems);
+  var receiptList = [];
+
+  receiptList.push({
+    receiptItems: receiptItems,
+    count: selectCount,
+    amount: priceTotal
+  });
+
+  localStorage.removeItem("receiptItems");
+}
+
+function deleteReceiptItem(id, receiptItems) {
+  receiptItems.forEach(function(receiptItem, index) {
+    if (receiptItem.item.id === id) {
+      receiptItems.splice(index - 1, 1);
+      localStorage.removeItem("receiptItems");
+      localStorage.setItem('receiptItems', JSON.stringify(receiptItems))
+    }
+  });
+
+  updateSelectCount(receiptItems);
+  updatepriceTotal(receiptItems);
+}
+
+function updateCart(receiptItems) {
   var itemHTML = '';
-  receiptItems.forEach(function(receiptItem){
+  receiptItems.forEach(function(receiptItem) {
     var item = receiptItem.item;
     itemHTML += '<tr id="' + item.id + '"><td class="goods">' + item.name + '</td><td class="price">' + item.price + '/' + item.unit + '</td><td class="count"><input class="count-input" type="text" value="1"/></td><td class="subTotal">' +
       item.price * receiptItem.count + '</td><td><button class="btn btn-default delete-button" type="submit" data-id="' + item.id + '">删除</button></td></tr>';
   });
   $('tbody').append(itemHTML);
+}
+
+function updateSelectCount(receiptItems) {
+  var selectedCount = receiptItems.length;
+  $("#selectedCount").text(selectedCount);
+}
+
+function updatepriceTotal(receiptItems) {
+  var priceTotal = 0;
+
+  receiptItems.forEach(function(receiptItem) {
+    priceTotal += receiptItem.item.price * receiptItem.count;
+  });
+
+  $("#priceTotal").text(priceTotal);
 }
